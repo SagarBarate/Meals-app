d4655f7029f03abc8fa37c588a699acec87edade
Sagar Barate <sagarbarate2907@com.com>
2023-06-18 15:37:39 +0530

    meal api

---
+++ index.js
@@ -0,0 +1,10 @@
+const search =document.getElementById('search');
+const submit =document.getElementById('submit');
+const random =document.getElementById('random');
+const mealEl =document.getElementById('meals');
+
+
+const resultHeading= document.getElementsByClassName('result-heading');
+const single_mealEl= document.getElementById('single-meal');
+
+//event listner
